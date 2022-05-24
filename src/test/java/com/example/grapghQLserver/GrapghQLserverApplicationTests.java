9d3d4de0b7778fd0e5eb5a585d9dd3b9793d76eb
Duelivotni <souji@DESKTOP-JFIPBC2>
2022-05-24 15:11:02 +0500

    test fix

--- src/test/java/com/example/grapghQLserver/GrapghQLserverApplicationTests.java
+++ src/test/java/com/example/grapghQLserver/GrapghQLserverApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.grapghQLserver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.graphQLserver.datafetchers.CustomerDataFetcher;
+import com.example.graphQLserver.datastore.CustomerRepository;
 import com.example.graphQLserver.datastore.CustomerService;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration;
@@ -18,7 +19,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import graphql.ExecutionResult;
 
-@SpringBootTest(classes = {DgsAutoConfiguration.class, CustomerDataFetcher.class, CustomerService.class})
+@SpringBootTest(classes = {
+	DgsAutoConfiguration.class, 
+	CustomerDataFetcher.class, 
+	CustomerService.class,
+	CustomerRepository.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GrapghQLserverApplicationTests {
 
@@ -27,33 +32,30 @@ class GrapghQLserverApplicationTests {
 
 	@Test
 	@Order(1)
+	void addCutomer_shouldAdd_andReturnNewCustomer() throws JSONException {
+		ExecutionResult executionResult = dgsQueryExecutor.execute(
+			"mutation {addCustomer(input : {fullName : \"John\"" +
+			" orders : [{name : \"Potatoe\" price : 15.55}] } )" + 
+			" {fullName orders {name price}}}"
+			);
+		String expectedResultAfterUpdate = "{\"addCustomer\":" +
+		"{\"fullName\":\"John\",\"orders\":[" +
+		"{\"price\":15.55,\"name\":\"Potatoe\"}" +
+		"]}}";
+
+		JSONObject actualResult = new JSONObject(executionResult.getData().toString());
+		assertEquals(expectedResultAfterUpdate, actualResult.toString());
+	}
+
+	@Test
+	@Order(2)
 	void getAllCustomers_shouldReturnFullList_ofCustomers() throws JSONException {
 		ExecutionResult executionResult = dgsQueryExecutor.execute(
 			"{getAllCustomers {fullName orders {name price}}}"
 			);
 		String expectedResult = "{\"getAllCustomers\":[" +
 			"{\"fullName\":\"John\",\"orders\":[" +
-			"{\"price\":15.55,\"name\":\"Potatoe\"}," +
-			"{\"price\":16.2,\"name\":\"Cucumber\"}]}," +
-			"{\"fullName\":\"Matt\",\"orders\":[" +
-			"{\"price\":12.6,\"name\":\"Tomato\"},"+
-			"{\"price\":16.2,\"name\":\"Carrot\"}" +
-			"]}]}";
-
-		JSONObject actualResult = new JSONObject(executionResult.getData().toString());
-		assertEquals(expectedResult, actualResult.toString());
-	}
-
-	@Test
-	@Order(2)
-	void getSomeCustomers_shouldReturnCustomers_withThisOrderName() throws JSONException {
-		ExecutionResult executionResult = dgsQueryExecutor.execute(
-			"{getCustomersWithOrder(orderName:\"Potatoe\") {fullName orders {name price }}}"
-			);
-		String expectedResult = "{\"getCustomersWithOrder\":[" +
-			"{\"fullName\":\"John\",\"orders\":[" +
-			"{\"price\":15.55,\"name\":\"Potatoe\"}," +
-			"{\"price\":16.2,\"name\":\"Cucumber\"}" +
+			"{\"price\":15.55,\"name\":\"Potatoe\"}" +
 			"]}]}";
 
 		JSONObject actualResult = new JSONObject(executionResult.getData().toString());
@@ -62,35 +64,35 @@ class GrapghQLserverApplicationTests {
 
 	@Test
 	@Order(3)
-	void addCutomer_shouldAdd_andReturnNewCustomer() throws JSONException {
+	void getSomeCustomers_shouldReturnCustomers_withThisOrderName() throws JSONException {
 		ExecutionResult executionResult = dgsQueryExecutor.execute(
-			"mutation {addCustomer(input : {fullName : \"newCustomerName\"" +
-			" orders : [{name : \"orderName\" price : 99.99}] } )" + 
-			" {fullName orders {name price}}}"
+			"{getCustomersWithOrder(orderName:\"Potatoe\") {fullName orders {name price }}}"
 			);
-		String expectedResultAfterUpdate = "{\"addCustomer\":" +
-		"{\"fullName\":\"newCustomerName\",\"orders\":[" +
-		"{\"price\":99.99,\"name\":\"orderName\"}" +
-		"]}}";
+		String expectedResult = "{\"getCustomersWithOrder\":[" +
+			"{\"fullName\":\"John\",\"orders\":[" +
+			"{\"price\":15.55,\"name\":\"Potatoe\"}" +
+			"]}]}";
 
 		JSONObject actualResult = new JSONObject(executionResult.getData().toString());
-		assertEquals(expectedResultAfterUpdate, actualResult.toString());
+		assertEquals(expectedResult, actualResult.toString());
 	}
 
 	@Test
 	@Order(4)
 	void removeCutomer_shouldDelete_andReturnCustomersList() throws JSONException {
+		dgsQueryExecutor.execute(
+			"mutation {addCustomer(input : {fullName : \"Bob\"" +
+			" orders : [{name : \"Carrot\" price : 15.55}] } )" + 
+			" {fullName orders {name price}}}"
+			);
+
 		ExecutionResult executionResultAfterDelete = dgsQueryExecutor.execute(
-			"mutation {removeCustomer(customerName : \"newCustomerName\") " +
+			"mutation {removeCustomer(customerName : \"John\") " +
 			"{fullName orders { name price } } }"
 			);
 		String expectedResultAfterDelete = "{\"removeCustomer\":[" +
-		"{\"fullName\":\"John\",\"orders\":[" +
-		"{\"price\":15.55,\"name\":\"Potatoe\"}," +
-		"{\"price\":16.2,\"name\":\"Cucumber\"}]}," +
-		"{\"fullName\":\"Matt\",\"orders\":[" +
-		"{\"price\":12.6,\"name\":\"Tomato\"},"+
-		"{\"price\":16.2,\"name\":\"Carrot\"}" +
+		"{\"fullName\":\"Bob\",\"orders\":[" +
+		"{\"price\":15.55,\"name\":\"Carrot\"}" +
 		"]}]}";
 
 		JSONObject actualResultAfterDelete = new JSONObject(executionResultAfterDelete.getData().toString());
